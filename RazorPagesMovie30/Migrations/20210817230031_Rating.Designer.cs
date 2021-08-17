86af999aa0f478794643378de41c7ec6c1050f11
Scot <bscot.hagen@gmail.com>
2021-08-17 16:03:26 -0700

    added new field, Rating

--- RazorPagesMovie30/Migrations/20210817230031_Rating.Designer.cs
+++ RazorPagesMovie30/Migrations/20210817230031_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie30.Data;
 
 namespace RazorPagesMovie30.Migrations
 {
     [DbContext(typeof(RazorPagesMovie30Context))]
-    partial class RazorPagesMovie30ContextModelSnapshot : ModelSnapshot
+    [Migration("20210817230031_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
